db7d2179107c0ff1a93aaa1c8845c72732de4380
oraclexudehua <2320838977@qq.com>
2020-01-16 13:55:55 +0800

    添加select自定义渲染功能

--- src/MushinySelect.tsx
+++ src/MushinySelect.tsx
@@ -1,45 +1,39 @@
 import { Select } from 'antd';
-import React, {Component } from 'react';
-import {
-  SelectProps
-} from 'antd/es/Select';
+import React, { Component } from 'react';
+import { SelectProps } from 'antd/es/Select';
 
 interface OptionsRecord {
   label: string;
   key: string;
-  value: any
+  value: any;
 }
-
 
 export interface MushinySelectProps extends SelectProps {
   optionsSources: Array<OptionsRecord>;
+  renderOption?: (element: any, index: number) => {};
 }
 
-class MushinySelect extends Component<MushinySelectProps>{
-
-
+class MushinySelect extends Component<MushinySelectProps> {
   renderOption = () => {
-    const { optionsSources } = this.props
-    const data_: Array<React.ReactNode> = []
+    const { optionsSources, renderOption } = this.props;
+    const data_: Array<React.ReactNode> = [];
     for (let index = 0; index < optionsSources.length; index++) {
       const element = optionsSources[index];
-      data_.push(
-        <Select.Option value={element.value} key={element.key}>
-          {element.label}
-        </Select.Option>
-      )
+      if (renderOption) {
+        data_.push(renderOption(element, index));
+      } else {
+        data_.push(
+          <Select.Option value={element.value} key={element.key}>
+            {element.label}
+          </Select.Option>,
+        );
+      }
     }
-    return data_
-  }
+    return data_;
+  };
 
   render(): React.ReactNode {
-    return (
-      <Select
-        {...this.props}
-      >
-        {this.renderOption()}
-      </Select>
-    )
+    return <Select {...this.props}>{this.renderOption()}</Select>;
   }
 }
 
